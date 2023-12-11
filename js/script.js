4ae951d0a1bf382d99d4dc0003640d959b7c1721
stappost <emanuele.scarpantoni@gmail.com>
2023-12-11 16:18:27 +0100

    aggiunto testo con l'interpolazione

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ createApp({
     // aggiungere in data il messaggio da voler stampare
     data(){
         return{
+            message: "Hello World",
             text: "<h1>Hello World</h1>",
             image: "nice.jpeg"
         }
